bf7e4ce7884d3b7cb120397b32711188a639eb1e
Jacob Gadikian <faddat@gmail.com>
2016-01-17 19:59:20 +0700

    Update env2conf.sh
    crosses fingers

--- env2conf.sh
+++ env2conf.sh
@@ -1,9 +1,9 @@
-echo "[default]" > $GOPATH/src/github.com/superordinate/kDaemon/config/app.conf
-echo "bind_ip = $BIND_IP" >> $GOPATH/src/github.com/superordinate/kDaemon/config/app.conf
-echo "api_port = $API_PORT" >> $GOPATH/src/github.com/superordinate/kDaemon/config/app.conf
-echo "ui_port = $UI_PORT" >> $GOPATH/src/github.com/superordinate/kDaemon/config/app.conf
-echo "mysql_host = $MYSQL_HOST" >> $GOPATH/src/github.com/superordinate/kDaemon/config/app.conf
-echo "mysql_user = $MYSQL_USER" >> $GOPATH/src/github.com/superordinate/kDaemon/config/app.conf
-echo "mysql_password = $MYSQL_PASSWORD" >> $GOPATH/src/github.com/superordinate/kDaemon/config/app.conf
-echo "mysql_dbname = $MYSQL_DBNAME" >> $GOPATH/src/github.com/superordinate/kDaemon/config/app.conf
-$GOPATH/bin/kdaemon
+echo "[default]" > config/app.conf
+echo "bind_ip = $BIND_IP" >> config/app.conf
+echo "api_port = $API_PORT" >> config/app.conf
+echo "ui_port = $UI_PORT" >> config/app.conf
+echo "mysql_host = $MYSQL_HOST" >> config/app.conf
+echo "mysql_user = $MYSQL_USER" >> config/app.conf
+echo "mysql_password = $MYSQL_PASSWORD" >> config/app.conf
+echo "mysql_dbname = $MYSQL_DBNAME" >> config/app.conf
+./kdaemon
