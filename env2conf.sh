0658b12cc0e99cc8e3cca0e8054a975df46b3084
Jacob Gadikian <faddat@gmail.com>
2016-01-17 20:17:22 +0700

    Update env2conf.sh

--- env2conf.sh
+++ env2conf.sh
@@ -1,4 +1,5 @@
 mkdir /kdaemon/config
+cd kdaemon
 echo "[default]" > /kdaemon/config/app.conf
 echo "bind_ip = $BIND_IP" >> /kdaemon/config/app.conf
 echo "api_port = $API_PORT" >> /kdaemon/config/app.conf
@@ -7,4 +8,5 @@ echo "mysql_host = $MYSQL_HOST" >> /kdaemon/config/app.conf
 echo "mysql_user = $MYSQL_USER" >> /kdaemon/config/app.conf
 echo "mysql_password = $MYSQL_PASSWORD" >> /kdaemon/config/app.conf
 echo "mysql_dbname = $MYSQL_DBNAME" >> /kdaemon/config/app.conf
+chmod a+x /kdaemon/kdaemon
 ./kdaemon
